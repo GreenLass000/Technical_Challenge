a4ac9886e820d90d07d93d8a5b4aaed5870ce6ab
GreenLass000 <marcosnavarromartin@gmail.com>
2025-05-08 13:42:31 +0200

    Created getTaskByIdHandler function

--- back/src/handlers/taskHandlers.ts
+++ back/src/handlers/taskHandlers.ts
@@ -12,3 +12,18 @@ export async function getTasksHandler(req: Request, res: Response): Promise<void
   }
 }
 
+export async function getTaskByIdHandler(req: Request, res: Response): Promise<void> {
+  const taskId = parseInt(req.params.id, 10);
+  try {
+	const task = await taskModel.getTaskById(taskId);
+	if (task) {
+	  res.json(task);
+	} else {
+	  res.status(404).json({ message: 'Task not found' });
+	}
+  } catch (e) {
+	console.log(e);
+	res.status(500).json({ message: "Error fetching task" });
+  }
+}
+
